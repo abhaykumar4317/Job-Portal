28d551eaa4f53b022daf75db2f275619073c2946
Satyam Singh <aicinema69@gmail.com>
2026-04-18 21:15:45 +0530

    added route in home page

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
+  
   resolve: {
     alias: {
       "@": fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+
   server: {
     historyApiFallback: true,
+    allowedHosts: ["harmful-curled-glimpse.ngrok-free.dev"],
   },
 })
